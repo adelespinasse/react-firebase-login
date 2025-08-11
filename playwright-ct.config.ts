aedd89dd7114acbe45b74b4cf12a89fb95595dca
Alan deLespinasse <alan@takemobi.com>
2025-08-11 17:58:41 -0400

    Playwright reporting

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig, devices } from '@playwright/experimental-ct-react';
+import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/experimental-ct-react';
+
+const reporters: PlaywrightTestConfig['reporter'] = [['list'], ['html', { open: 'never' }]];
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -18,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : [['list'], ['html', { open: 'never' }]],
+  reporter: process.env.CI ? [['github'], ...reporters] : reporters,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
