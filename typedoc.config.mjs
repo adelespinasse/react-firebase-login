e4a1b24aaa3694db7afcbccf0d79292609228136
Alan deLespinasse <alan@takemobi.com>
2025-08-23 15:05:02 -0400

    fix: changelog and stuff

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -1,11 +1,19 @@
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
+
+const versionLinkText = process.env.LATEST_RELEASE || "Changelog";
+
 const config = {
   entryPoints: ['lib/index.ts'],
+  alwaysCreateEntryPointModule: false,
   readme: 'DOCS.md',
   navigationLinks: {
-    'Email author': 'mailto:adelespinasse@gmail.com',
-    'Author\'s home page': 'https://aldel.com',
+    [versionLinkText]: 'https://www.npmjs.com/package/@aldel/react-firebase-login',
+    'Email aldel': 'mailto:adelespinasse@gmail.com',
+    'aldel home page': 'https://aldel.com',
   },
+  projectDocuments: ['CHANGELOG.md'],
+  kindSortOrder: ['Function', 'Variable', 'Class', 'Interface', 'TypeAlias'],
+  searchInDocuments: true,
 }
 
 export default config;
